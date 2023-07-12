234a714159f23f600807b77e2ae9f98d3d0d8006
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-07-12 13:58:34 -0400

    Create tl_news.php

---
+++ system/modules/frasch_news_main_image/dca/tl_news.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Frasch News Main Image
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/frasch-news-main-image
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+ /* Extend the tl_news palettes */
+$GLOBALS['TL_DCA']['tl_news']['palettes']['default'] = str_replace(';{teaser_legend}', ';{add_news_fields_legend},news_issue;{teaser_legend}', $GLOBALS['TL_DCA']['tl_news']['palettes']['default']);
+
+$GLOBALS['TL_DCA']['tl_news']['fields']['news_issue'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_news']['news_issue'],
+	'inputType'               => 'text',
+	'default'                 => '',
+	'search'                  => true,
+	'eval'                    => array('mandatory'=>false, 'tl_class'=>'w50'),
+	'sql'                     => "varchar(255) NOT NULL default ''"
+);
