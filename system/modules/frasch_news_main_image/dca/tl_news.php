72243117fe84712cec04638126de8eb93b144967
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-07-12 15:05:42 -0400

    Update tl_news.php

--- system/modules/frasch_news_main_image/dca/tl_news.php
+++ system/modules/frasch_news_main_image/dca/tl_news.php
@@ -11,13 +11,63 @@
 **/
 
  /* Extend the tl_news palettes */
-$GLOBALS['TL_DCA']['tl_news']['palettes']['default'] = str_replace(';{image_legend}', ';{reader_main_image_legend},reader_main_image;{image_legend}', $GLOBALS['TL_DCA']['tl_news']['palettes']['default']);
+$GLOBALS['TL_DCA']['tl_news']['palettes']['default'] = str_replace(';{image_legend}', ';{reader_main_image_legend},addImageMainImage;{image_legend}', $GLOBALS['TL_DCA']['tl_news']['palettes']['default']);
 
-$GLOBALS['TL_DCA']['tl_news']['fields']['reader_main_image'] = array
+
+$GLOBALS['TL_DCA']['tl_news']['subpalettes']['addImageMainImage'] = 'singleSRCMainImage,fullsizeMainImage,sizeMainImage,floatingMainImage,overwriteMetaMainImage';
+
+$GLOBALS['TL_DCA']['tl_news']['fields']['addImageMainImage'] = array
 (
-    'label'                   => &$GLOBALS['TL_LANG']['tl_news']['reader_main_image'],
+    'inputType'               => 'checkbox',
+    'eval'                    => array('submitOnChange'=>true),
+    'sql'                     => array('type' => 'boolean', 'default' => false)
+);
+
+
+
+$GLOBALS['TL_DCA']['tl_news']['fields']['singleSRCMainImage'] = array
+(
+    'label'                   => &$GLOBALS['TL_LANG']['tl_news']['singleSRCMainImage'],
     'inputType'               => 'fileTree',
     'exclude'                 => true,
     'eval'                    => array('fieldType'=>'radio', 'filesOnly'=>true, 'extensions'=>'%contao.image.valid_extensions%', 'mandatory'=>true),
     'sql'                     => "binary(16) NULL"
+);
+
+$GLOBALS['TL_DCA']['tl_news']['fields']['fullsizeMainImage'] = array
+(
+    'label'                   => &$GLOBALS['TL_LANG']['tl_news']['fullsizeMainImage'],
+    'inputType'               => 'checkbox',
+    'eval'                    => array('tl_class'=>'w50'),
+    'sql'                     => array('type' => 'boolean', 'default' => false)
+);
+
+$GLOBALS['TL_DCA']['tl_news']['fields']['sizeMainImage'] = array
+(
+    'label'                   => &$GLOBALS['TL_LANG']['MSC']['sizeMainImage'],
+    'inputType'               => 'imageSize',
+    'reference'               => &$GLOBALS['TL_LANG']['MSC'],
+    'eval'                    => array('rgxp'=>'natural', 'includeBlankOption'=>true, 'nospace'=>true, 'helpwizard'=>true, 'tl_class'=>'w50 clr'),
+    'options_callback' => static function () {
+        return System::getContainer()->get('contao.image.sizes')->getOptionsForUser(BackendUser::getInstance());
+    },
+    'sql'                     => "varchar(64) NOT NULL default ''"
+);
+
+$GLOBALS['TL_DCA']['tl_news']['fields']['floatingMainImage'] = array
+(
+    'label'                   => &$GLOBALS['TL_LANG']['tl_news']['floatingMainImage'],
+    'inputType'               => 'radioTable',
+    'options'                 => array('above', 'left', 'right', 'below'),
+    'eval'                    => array('cols'=>4, 'tl_class'=>'w50'),
+    'reference'               => &$GLOBALS['TL_LANG']['MSC'],
+    'sql'                     => "varchar(12) NOT NULL default 'above'"
+);
+
+$GLOBALS['TL_DCA']['tl_news']['fields']['overwriteMetaMainImage'] = array
+(
+    'label'                   => &$GLOBALS['TL_LANG']['tl_news']['overwriteMetaMainImage'],
+    'inputType'               => 'checkbox',
+    'eval'                    => array('submitOnChange'=>true, 'tl_class'=>'w50 clr'),
+    'sql'                     => array('type' => 'boolean', 'default' => false)
 );
