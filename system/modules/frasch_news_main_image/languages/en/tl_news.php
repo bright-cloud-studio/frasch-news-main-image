97844b9fb0a9ce7d2daab41130b0cbabba255a75
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-07-12 13:59:59 -0400

    Create tl_news.php

---
+++ system/modules/frasch_news_main_image/languages/en/tl_news.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Frasch News Main Image
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/frasch-news-main-image
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+/* Legends */
+$GLOBALS['TL_LANG']['tl_news']['add_news_fields_legend'] = 'Custom Field\'s Settings';
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_news']['news_issue']          = array('Issue Number', 'Enter this news article\'s issue number');
