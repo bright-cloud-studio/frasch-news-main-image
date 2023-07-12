f03fd9db021ad3d8135de55ad12967a5cbbbfbb3
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-07-12 14:53:24 -0400

    Update tl_news.php

--- system/modules/frasch_news_main_image/languages/en/tl_news.php
+++ system/modules/frasch_news_main_image/languages/en/tl_news.php
@@ -11,7 +11,7 @@
 **/
 
 /* Legends */
-$GLOBALS['TL_LANG']['tl_news']['reader_main_iamge_legend'] = 'Reader Main Image';
+$GLOBALS['TL_LANG']['tl_news']['reader_main_image_legend'] = 'Reader Main Image';
 
 /* Fields */
 $GLOBALS['TL_LANG']['tl_news']['reader_main_image']          = array('Reader Main Image', 'Select the image to be used at the main image in the Reader module');
