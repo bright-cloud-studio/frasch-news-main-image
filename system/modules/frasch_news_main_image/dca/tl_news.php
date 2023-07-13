3a71012667ac4653a02c3dce359cc8d80c00cfb5
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-07-13 08:36:35 -0400

    Update tl_news.php

--- system/modules/frasch_news_main_image/dca/tl_news.php
+++ system/modules/frasch_news_main_image/dca/tl_news.php
@@ -11,23 +11,22 @@
 **/
 
  /* Extend the tl_news palettes */
-$GLOBALS['TL_DCA']['tl_news']['palettes']['default'] = str_replace(';{image_legend}', ';{reader_main_image_legend},addImageMainImage;{image_legend}', $GLOBALS['TL_DCA']['tl_news']['palettes']['default']);
+$GLOBALS['TL_DCA']['tl_news']['palettes']['default'] = str_replace(';{enclosure_legend:hide}', ';{reader_main_image_legend},addImageMainImage;{enclosure_legend:hide}', $GLOBALS['TL_DCA']['tl_news']['palettes']['default']);
 
 $GLOBALS['TL_DCA']['tl_news']['palettes']['__selector__'] = array('source', 'addImageMainImage', 'addImage', 'addEnclosure', 'overwriteMeta');
 $GLOBALS['TL_DCA']['tl_news']['subpalettes']['addImageMainImage'] = 'singleSRCMainImage,sizeMainImage';
 
 $GLOBALS['TL_DCA']['tl_news']['fields']['addImageMainImage'] = array
 (
+    'exclude'                 => true,
     'inputType'               => 'checkbox',
     'eval'                    => array('submitOnChange'=>true),
-    'sql'                     => array('type' => 'boolean', 'default' => false)
+    'sql'                     => "char(1) NOT NULL default ''"
 );
-
-
 
 $GLOBALS['TL_DCA']['tl_news']['fields']['singleSRCMainImage'] = array
 (
-    'label'                   => &$GLOBALS['TL_LANG']['tl_content']['singleSRC'],
+    'label'                   => &$GLOBALS['TL_LANG']['tl_content']['singleSRCMainImage'],
     'exclude'                 => true,
     'inputType'               => 'fileTree',
     'eval'                    => array('fieldType'=>'radio', 'filesOnly'=>true, 'extensions'=>'%contao.image.valid_extensions%', 'mandatory'=>true),
@@ -36,13 +35,14 @@ $GLOBALS['TL_DCA']['tl_news']['fields']['singleSRCMainImage'] = array
 
 $GLOBALS['TL_DCA']['tl_news']['fields']['sizeMainImage'] = array
 (
-    'label'                   => &$GLOBALS['TL_LANG']['MSC']['imgSize'],
+    'label'                   => &$GLOBALS['TL_LANG']['MSC']['sizeMainImage'],
+    'exclude'                 => true,
     'inputType'               => 'imageSize',
     'reference'               => &$GLOBALS['TL_LANG']['MSC'],
-    'eval'                    => array('rgxp'=>'natural', 'includeBlankOption'=>true, 'nospace'=>true, 'helpwizard'=>true, 'tl_class'=>'w50 clr'),
-    'options_callback' => static function () {
+    'eval'                    => array('rgxp'=>'natural', 'includeBlankOption'=>true, 'nospace'=>true, 'helpwizard'=>true, 'tl_class'=>'w50'),
+    'options_callback' => static function ()
+    {
         return System::getContainer()->get('contao.image.sizes')->getOptionsForUser(BackendUser::getInstance());
     },
     'sql'                     => "varchar(64) NOT NULL default ''"
 );
-
